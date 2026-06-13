19aab10ef7570683537053c6f70d28c8282c178b
Sadiq <sadiq0698@gmail.com>
2026-06-13 17:57:05 +0600

    feat: fix class path

--- service.sh
+++ service.sh
@@ -2,4 +2,4 @@
 
 exec >> /data/local/tmp/lockscreen-powermenu-blocker.log 2>&1
 
-exec su -lp 1000 -c "CLASSPATH=/system/usr/share/mytool/mytool.jar app_process /system/bin BlockerService"
+exec su -lp 1000 -c "CLASSPATH=/system/usr/share/lockscreen-powermenu-blocker/BlockerService.jar app_process /system/bin BlockerService"
